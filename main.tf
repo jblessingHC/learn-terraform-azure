9b643c538ca2acb75cef7769351fd106a4e7456b
jblessingHC <85644461+jblessingHC@users.noreply.github.com>
2022-01-11 17:16:34 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,41 @@
+# Configure the Azure provider
+terraform {
+  backend "remote" {
+    organization = "jblessing-training"
+
+    workspaces {
+      name = "AZExample-Workspace"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "westus2"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
+}
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
